022ff20981c4ac611a90f6c9ac949b857f85af1b
nguyentantin2105 <93tinndu1314@gmail.com>
2021-12-14 11:44:09 +0700

    init project

---
+++ components/Sidebar.js
@@ -0,0 +1,24 @@
+import {HomeIcon, LibraryIcon, SearchIcon} from "@heroicons/react/outline";
+
+function Sidebar() {
+    return (
+        <div>
+            <div className="text-gray-500">
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <HomeIcon className="h-5 w-5 "/>
+                    <p>Home</p>
+                </button>
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <SearchIcon className="h-5 w-5 "/>
+                    <p>Search</p>
+                </button>
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <LibraryIcon className="h-5 w-5 "/>
+                    <p>Your Library</p>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
