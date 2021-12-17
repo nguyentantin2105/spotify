45b13afea6d7dea7cd5fb0c4c9d1d3d4beee9ed2
nguyentantin2105 <93tinndu1314@gmail.com>
2021-12-17 12:10:34 +0700

    Update sidebar UI

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,13 @@ import Sidebar from '../components/Sidebar'
 export default function Home() {
   return (
     <div className="">
-      <main>
-        <Sidebar />
+      <main className="bg-black h-screen overflow-hidden">
+        <Sidebar /> 
       </main>
+
+      <div>
+        {/* Player */}
+      </div>
     </div>
   )
 }
